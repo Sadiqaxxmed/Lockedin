73d458fb52fcc05fd91b97bd572975ddf7220d26
Sadiq Ahmed <43020644+Sadiqaxxmed@users.noreply.github.com>
2023-04-18 15:18:22 -0700

    Almost done wit auth

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+  return (
+    <>
+      <div className="NV-div-main">
+        <div className="NV-div-left">
+          <NavLink exact to="/">
+            <i class="fa-brands fa-linkedin fa-2xl"></i>
+          </NavLink>
+          <input placeholder='Search' className="NV-search"></input>
+        </div>
+
+        <div className="NV-div-right">
+          {isLoaded && <ProfileButton user={sessionUser} />}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navigation;
