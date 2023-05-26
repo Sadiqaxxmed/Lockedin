93cd468606f96efba81380fc34cbfe6bc0d8113e
Sadiq Ahmed <offsznahmed@Sadiqs-MacBook-Pro.local>
2023-05-26 13:51:52 -0700

    Finished user CRUD backend routes.

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User
+from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
 
@@ -23,3 +23,57 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+
+@user_routes.route('/<int:id>/about', methods=['POST'])
+@login_required
+def add_user_about(id):
+    """
+    Add the 'about' column to an existing user who doesn't have one
+    """
+    user = User.query.get(id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    if user.about:
+        return jsonify({'message': 'User already has an about column'}), 400
+
+    about = request.json.get('about')
+    if not about:
+        return jsonify({'message': 'About content is required'}), 400
+
+    user.about = about
+    db.session.commit()
+
+    return jsonify({'message': 'About added to user successfully'})
+
+@user_routes.route('/<int:id>/about', methods=['PUT'])
+@login_required
+def update_user_about(id):
+    """
+    Update the 'about' column of a user
+    """
+    user = User.query.get(id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    about = request.json.get('about')
+    user.about = about
+    db.session.commit()
+
+    return jsonify({'message': 'User about updated successfully'})
+
+@user_routes.route('/<int:id>/about', methods=['DELETE'])
+@login_required
+def delete_user_about(id):
+    """
+    Delete the 'about' column of a user
+    """
+    user = User.query.get(id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    user.about = ''
+    db.session.commit()
+
+    return jsonify({'message': 'User about deleted successfully'})
